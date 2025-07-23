87d1d9e93e810ca274863c28c7b8f75ea3460c9f
cgarcia1196 <cgarcia1196@gmail.com>
2025-07-23 16:35:02 -0700

    added jacquelines login to my project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const NavBar = ({ onSelect }) => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        toast.success('Logged out!');
+        navigate('/login')
+      };
+
   return (
     <nav className="bg-gray-800 p-4 shadow-md">
         <div className="max-w-7xl mx-auto flex space-x-6 text-white">
@@ -9,7 +18,7 @@ const NavBar = ({ onSelect }) => {
             <button className="hover:text-yellow-400" onClick={() => onSelect("experience")}>Experience</button>
             <button className="hover:text-yellow-400" onClick={() => onSelect("projects")}>Projects</button>
             <button className="hover:text-yellow-400" onClick={() => onSelect("skills")}>Skills</button>
-
+            <button className="hover:text-yellow-400 ml-auto" onClick={() => handleLogout()}>Logout</button>
         </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import PortfolioPage from './pages/PortfolioPage.jsx'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import Login from './components/Login.jsx'
+import ProtectedRoute from './components/protectedRoute.jsx'
 
 const router = createBrowserRouter([
   {path: '/', element: <App/>},
-  {path: '/portfolio', element: <PortfolioPage/>},
+  {
+    path: '/portfolio', 
+    element: (
+      <ProtectedRoute>
+        <PortfolioPage/>
+      </ProtectedRoute>
+    )
+  },
+      
+  { path: '/login', element: <Login/> },
   {path: '/*', element: <div>404 not found</div>}
 ])
 
